5e9c95133d855bee5973b9cc9adb61bfaca1bee2
m4r1no0 <marinoosorio97@gmail.com>
2026-05-14 14:22:48 -0500

    frontStable

--- js/main.js
+++ js/main.js
@@ -7,10 +7,7 @@ import { initInforme } from "./informes.js";
 import { initContacto } from "./contacto.js";
 import { initPoliza } from "./poliza.js";
 import { initPago } from "./pago.js";
-<<<<<<< HEAD
-=======
 import { initAreaFormacion } from "./areaFormacion.js";
->>>>>>> 1e07be78a3d1e1784ca46af8d5bb05c8d9c5bab7
 
 console.log("=== MAIN.JS CARGADO ===");
 
@@ -28,12 +25,8 @@ const inicializadores = {
     'direccion.html': initDireccion,
     'contacto.html': initContacto,
     'poliza.html': initPoliza,
-<<<<<<< HEAD
-    'pagos.html': initPago
-=======
     'pagos.html': initPago,
     'area_formacion.html': initAreaFormacion
->>>>>>> 1e07be78a3d1e1784ca46af8d5bb05c8d9c5bab7
 };
 
 document.addEventListener("click",   function (e) {
